4be59c3ee364c2cf90d52fefc0f2215ace1f9f9f
Jorge Edson <jorge.edson.bsi@live.com>
2025-03-18 22:34:45 -0300

    codigo inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sentinel.Alertas.Acoes
---
+++ Sentinel.Alertas.Acoes/Form1.cs
@@ -0,0 +1,10 @@
+namespace Sentinel.Alertas.Acoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
